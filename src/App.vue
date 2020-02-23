e5276f4d6f99698d5aac6561dc0efbd8e3cc329a
youko <1602840538@qq.com>
2020-02-23 17:11:56 +0800

    init gshop

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="app">
+  <router-view/>
+  <FooterGuide v-show="$route.meta.showFooter"/>
+</div>
+
+
+</template>
+
+<script>
+  import FooterGuide from './components/FooterGuide/FooterGuide'
+    export default {
+        name: "App",
+        components: {
+          FooterGuide
+        }
+    }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+.a
+  width 100%
+  height 100%
+  background #f5f5f5
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,52 @@
+/* 路由*/
+import Vue from 'vue'
+import VueLoader from 'vue-router'
+
+Vue.use(VueLoader)
+
+import MSite from '../pages/MSite/MSite'
+import Order from '../pages/Order/Order'
+import Profile from '../pages/Profile/Profile'
+import Search from '../pages/Search/Search'
+import Login from '../pages/Login/Login'
+
+export default new VueLoader({
+  routes: [
+    {
+      path: '/msite',
+      component: MSite,
+      meta: {
+          showFooter: true
+      }
+    },
+    {
+      path: '/order',
+      component: Order,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/profile',
+      component: Profile,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/search',
+      component: Search,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/',
+      redirect: '/msite'
+    },
+    {
+      path: '/login',
+      component: Login
+    }
+  ]
+})
